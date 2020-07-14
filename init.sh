65cc035721f3b567f710b01ffa6cbc935bbafa39
harrykulkin <41946314+harrykulkin@users.noreply.github.com>
2020-07-14 23:16:47 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ echo "Let me check out one of KWEB service."
 read -p "Please, enter SVN Username : " svn_name
 read -p "Please, enter KWEB service (project) name : " service_name
 
-svn co svn://10.0.159.22:443/kweb/$service_name $script_dir/$service_name --username $svn_name
+svn co svn://{SVN 주소}/$service_name $script_dir/$service_name --username $svn_name
 mvn clean -f $script_dir/$service_name/pom.xml
 mvn package -f $script_dir/$service_name/pom.xml
 
